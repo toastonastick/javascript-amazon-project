9c21be8f4aa4c48cb71289e44ea520b8d4dd0d68
toastonastick <jpea42@gmail.com>
2025-04-02 00:15:12 -0500

    add item quantity

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -105,6 +105,14 @@ document.querySelectorAll('.js-delete-link')
       const container = document.querySelector(`.js-cart-item-container-${productId}`);
 
       container.remove();
-
     });
 });
+
+let cartQuantity = 0;
+
+cart.forEach((cartItem) => {
+  cartQuantity += cartItem.quantity;
+});
+
+document.querySelector('.js-return-to-home-link')
+  .innerHTML = `${cartQuantity} items`;
